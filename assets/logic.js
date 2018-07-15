6a968d7420dba03ddb25a2b393ff7c00f3f7d493
Mkelsey <mkh13b@my.fsu.edu>
2018-07-15 14:45:22 -0400

    changed wording

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $("#submit-btn").click(function() {
-        alert("You are an amazing individual full of love and light. You deserve to be loved and respected. There are people in this world who want nothing but the best for you. They see your success and your struggles. They're constantly cheering you on.")
+        alert("You are an amazing individual full of love and light. You deserve to be cherished and respected. There are people in this world who want nothing but the best for you. They see your successes and your struggles. They're constantly cheering you on.")
     });
 });
